0255939453426ea927b8d98db5f7aedf6cacc0ad
Mart Kop <mkop@avans.nl>
2018-03-06 10:09:54 +0100

    subscribe to newsletter added in form

--- src/main/java/avans/ivh11/mart/demo/Domain/NewsletterSMS.java
+++ src/main/java/avans/ivh11/mart/demo/Domain/NewsletterSMS.java
@@ -9,14 +9,14 @@ import org.slf4j.LoggerFactory;
 public class NewsletterSMS extends NewsletterFramework {
 
     //THESE ARE PRODUCTION CREDENTIALS
-//    public static final String ACCOUNT_SID = "ACca192ebce23f2dffed78bb23e9ddbbf3";
-//    public static final String FROM = "+3197014200826";
-//    public static final String AUTH_TOKEN = "289ac7db647dd82b5cf957311d55c428";
+    public static final String ACCOUNT_SID = "ACca192ebce23f2dffed78bb23e9ddbbf3";
+    public static final String FROM = "+3197014200826";
+    public static final String AUTH_TOKEN = "289ac7db647dd82b5cf957311d55c428";
 
     //THESE ARE TEST CREDENTIALS
-    public static final String ACCOUNT_SID = "AC994964b3d9e7aed3be62600c6a0943f6";
-    public static final String AUTH_TOKEN = "e2bc0065642db0096b7b18b455361031";
-    public static final String FROM = "+15005550006";
+//    public static final String ACCOUNT_SID = "AC994964b3d9e7aed3be62600c6a0943f6";
+//    public static final String AUTH_TOKEN = "e2bc0065642db0096b7b18b455361031";
+//    public static final String FROM = "+15005550006";
 //    public static final String FROM_INVALID = "+15005550001";
 //    public static final String FROM_SMS_INCAPABLE = "+15005550007";
 //    public static final String FROM_FULL_QUEUE = "+15005550008";
@@ -33,7 +33,8 @@ public class NewsletterSMS extends NewsletterFramework {
             try {
                 Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
                 Message message = Message.creator(
-                        new PhoneNumber(user.getPhone()),
+//                        new PhoneNumber(user.getPhone()),
+                        new PhoneNumber(TO),
                         new PhoneNumber(FROM),
                         newsletter.getBody()
                 ).create();
