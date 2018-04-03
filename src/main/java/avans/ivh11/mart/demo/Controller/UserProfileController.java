105271923ffd8591c267332103ab1d81c2dcc658
Mart Kop <mkop@avans.nl>
2018-04-03 12:10:01 +0200

    fixed - get user role by request, not custom functionality

--- src/main/java/avans/ivh11/mart/demo/Controller/UserProfileController.java
+++ src/main/java/avans/ivh11/mart/demo/Controller/UserProfileController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/profile")
 public class UserProfileController {
@@ -18,11 +20,11 @@ public class UserProfileController {
     private UserService userService;
 
     @GetMapping(value = "{id}")
-    public ModelAndView view(@PathVariable("id") String userId) {
+    public ModelAndView view(@PathVariable("id") String userId, HttpServletRequest request) {
         RegisteredUser user = this.userService.getUserById(Long.parseLong(userId));
         ModelAndView mav = new ModelAndView();
 
-        if (userService.getCurrentUser().getId() != Long.parseLong(userId) && !userService.getRole().equals(RegisteredUser.ROLE_SUPER_ADMIN)) {
+        if (userService.getCurrentUser().getId() != Long.parseLong(userId) &&!request.isUserInRole(RegisteredUser.ROLE_SUPER_ADMIN)) {
             mav.setViewName("error/403");
             mav.addObject("exception", new AccessDeniedException("You do not have access to this page"));
             return mav;
